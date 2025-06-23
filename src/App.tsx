d159d59536e398ac2a0ece411bea2fe05242f83e
duyhoangto <hoangtdse170030@fpt.edu.vn>
2025-06-23 20:55:40 +0700

    fixn lan 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { HomePage } from './pages/HomePage';
-import { SearchPage } from './pages/SearchPage';
-import { MovieDetailsPage } from './pages/MovieDetailsPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { HomePage } from "./pages/HomePage";
+import { SearchPage } from "./pages/SearchPage";
+import { MovieDetailsPage } from "./pages/MovieDetailsPage";
 
 function App() {
   return (
